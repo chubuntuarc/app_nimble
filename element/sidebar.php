f942505326009969b26005a61fc7679c6ad04c48
dev.jesusarc <dev.jesusarc@gmail.com>
2018-04-30 13:05:17 -0400

    Adjustments

--- element/sidebar.php
+++ element/sidebar.php
@@ -1,11 +1,10 @@
 <link rel="stylesheet" href="../app_nimble/css/sidebar.css">
 <ul id="slide-out" class="side-nav">
     <li><div class="user-view" <?php echo 'style="background-color: '.$main_color.'"' ?>>
-      <a href="#!user"><img class="circle responsive-img" src="images/profile/us1.gif"></a>
-      <a href="#!name"><span class="white-text name">Jesus Arciniega</span></a>
+      <a href="#!name"><span class="white-text name" style="margin-top: -16px;">Jesus Arciniega</span></a>
       <a href="#!email"><span class="white-text email">dev.jesusarc@gmail.com</span></a>
     </div></li>
-  <li><a href="stock.php"><i class="material-icons">storage</i>Inventario</a></li>
+  <li><a href="/nimble_pos"><i class="material-icons">desktop_mac</i>Punto de venta</a></li>
     <li><div class="divider"></div></li>
     <li><a class="waves-effect" href="/" >Mi cuenta</a></li>
     <li><a class="subheader">JesusArc 2018</a></li>
