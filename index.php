62f56b188f80a5d73765a129a75587002d8a9680
dev.jesusarc <dev.jesusarc@gmail.com>
2018-04-30 13:16:03 -0400

    Basic layout

--- index.php
+++ index.php
@@ -9,8 +9,44 @@ echo '<link rel="stylesheet" href="css/cards.css">';
 include_once 'element/navbar.php';
 //Side bar
 include_once 'element/sidebar.php';
-//Index html tags
-echo '<h1>Hola mundo</h1>';
+//html tags
+echo '<div class="row" style="height: 60px;margin-top: 60px;background-color: '.$main_color.';"></div>';
+?>
+
+<div class="row">
+  <div class="col s12 m3">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="images/pos.PNG">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>Modulo de punto de venta.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m9">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="images/pos.PNG">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <h5>Más información</h5>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
 //Footer
 include 'element/footer.php';
-?>       
+?>
